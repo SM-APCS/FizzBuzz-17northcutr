59f1f2e91b67498af4caaac13cded01237f9bdaa
T.Rex Northcut <17northcutr@smtexas.org>
2016-09-30 13:44:41 -0500

    I made the code

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fizzbuzz;
 
 /**
  *
- * @author Mr. T - CHANGE TO YOUR NAME
+ * @author T.Rex Northcut
  * @class AP CSA
  */
 public class FizzBuzz {
@@ -17,6 +13,20 @@ public class FizzBuzz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        for(int i=1; i<100; i++){
+            if(i%3 == 0){
+                if(i%5 == 0){
+                    System.out.println("FizzBuzz");
+                }
+                System.out.println("Fizz");
+            }
+            else if(i%5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
     }
     
 }
