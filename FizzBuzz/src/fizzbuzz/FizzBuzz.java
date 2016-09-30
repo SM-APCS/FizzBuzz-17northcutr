39b8bc566aa6a473c21de3955c6728c3a6cd470d
T.Rex Northcut <17northcutr@smtexas.org>
2016-09-30 13:49:29 -0500

    Commenting
    There, I commented the extremely simple code that probably shouldn't need to be commented

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -13,17 +13,18 @@ public class FizzBuzz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for(int i=1; i<100; i++){
-            if(i%3 == 0){
-                if(i%5 == 0){
+        for(int i=1; i<100; i++){ //for loop to count to 100 from 1
+            if(i%3 == 0){ //checks if i is divisible by 3
+                if(i%5 == 0){ //if i is also divisible by 5 print FizzBuzz
                     System.out.println("FizzBuzz");
                 }
+                //else just print Fizz
                 System.out.println("Fizz");
             }
-            else if(i%5 == 0){
+            else if(i%5 == 0){ //if i wasn't divisible by 3, but is divisible by 5 print Buzz
                 System.out.println("Buzz");
             }
-            else{
+            else{ //otherwise just print the number
                 System.out.println(i);
             }
         }
